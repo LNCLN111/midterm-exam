3a241ab7091a8d77580a0cb8c43d0a60e4300e19
DELIGERO G. LYNCOLIN <2202108@cc.usls.edu>
2025-02-17 09:23:39 +0800

    Feat: Create categories and items tables migration

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,15 +1,45 @@
-Hi. I'm Lynn from Las Vegas. Mowdels.
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
 
-Were hiring new promohtional mowdels to work en Las Vegas, Yuwezay.
+<form action="login.php" method="post">
+    <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+        <div class="relative bg-white pt-10 pb-8 px-10 shadow-xl mx-auto w-96 rounded-lg">
+            <div class="divide-y divide-gray-300/50 w-full">
+                <div class="space-y-6 py-8 text-base  text-gray-600">
 
-Are you etin to tweynty one yirs old?
+                    <?php if (isset($_GET['message'])): ?>
+                        <p class="text-sm text-red-500"><?= $_GET['message']; ?></p>
+                    <?php endif; ?>
 
-Do you have gewd english iskillz?
+                    <p class="text-xl font-medium leading-7">PHP Login System</p>
+                    <div class="space-y-4 flex flex-col">
+                        <input type="text"
+                               name="username"
+                               placeholder="Username"
+                               class="border border-gray-300/50 p-1 rounded focus:outline-none"/>
 
-A plesing fersonality and desar for an exciting new life.
+                        <input type="password"
+                               name="password"
+                               placeholder="Password"
+                               class="border border-gray-300/50 p-1 rounded focus:outline-none"/>
+                    </div>
+                </div>
+                <div class="pt-8 text-base font-semibold leading-7">
+                    <button type="submit" class="bg-sky-500 hover:bg-sky-600 px-4 py-1 text-white rounded">
+                        Login
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
 
-Ep so, dees offurtunity mee bee for you.
 
-If dis sounds intereseting, please emel us so we can errends a personal enterview.
-
-Tengs.
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,5 @@ Route::get('/', function () {
 Route::get(uri: '/login', action:'App\Http\Controllers\LoginController@index');
 
 Route::get(uri: '/item', action:'App\Http\Controllers\ItemController@index');
+
+Route::get(uri: '/form', action:'App\Http\Controllers\FormController@index');
