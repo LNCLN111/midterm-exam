e883d900ebf480b372be739e0d733e343b9b732e
DELIGERO G. LYNCOLIN <2202108@cc.usls.edu>
2025-02-24 08:48:02 +0800

    Hello world

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route::get(uri: '/login', action:'App\Http\Controllers\LoginController@index');
 Route::get(uri: '/item', action:'App\Http\Controllers\ItemController@index');
 
 Route::get(uri: '/form', action:'App\Http\Controllers\FormController@index');
+
+Route::get(uri: '/hello', action:'App\Http\Controllers\HelloController@index');
